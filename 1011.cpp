4e6c9efa2046f5f545f1caab45041965b3308c5f
lexilii <735110965@qq.com>
2019-01-22 22:04:00 +0800

    Update 1011.cpp

--- 1011.cpp
+++ 1011.cpp
@@ -1,4 +1,6 @@
 /*
+1011 World Cup Betting （20 分）
+
 With the 2010 FIFA World Cup running, football fans the world over were becoming increasingly excited as the best players from the best teams doing battles for the World Cup trophy in South Africa. Similarly, football betting fans were putting their money where their mouths were, by laying all manner of World Cup bets.
 
 Chinese Football Lottery provided a "Triple Winning" game. The rule of winning was simple: first select any three of the games. Then for each selected game, bet on one of the three possible results -- namely W for win, T for tie, and L for lose. There was an odd assigned to each result. The winner's odd would be the product of the three odds times 65%.
